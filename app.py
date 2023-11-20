ad20a4d8c3d950bcfd2219701821a1079c6d73ca
Daniel <danisalazar03@live.com>
2023-11-19 23:36:11 -0600

    new test with cosmos db connection

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+import json
+import os
+import sys
+import uuid
+
+from azure.core.exceptions import AzureError
+from azure.cosmos import CosmosClient, PartitionKey
+
+ENDPOINT = os.environ["COSMOS_ENDPOINT"]
+KEY = os.environ["COSMOS_KEY"]
+DATABASE_ID = "dbtest"
+
+client = CosmosClient(url=ENDPOINT, credential=KEY)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'ok'
+    try:
+        database = client.create_database_if_not_exists(id=DATABASE_ID)
+        return f"Database created or returned: {database.id}"
+
+    except:
+        return "Request to the Azure Cosmos database service failed."
 
 if __name__ == '__main__':
     app.run(debug=True)
